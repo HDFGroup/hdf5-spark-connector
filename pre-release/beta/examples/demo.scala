d8c9ea7f4fee9373b3391dff883a5fed614d5dc3
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2017-09-07 11:50:56 -0500

    SPARKY-8:added demo program that matches Apache documentation.

---
+++ pre-release/beta/examples/demo.scala
@@ -0,0 +1,28 @@
+// Copyright (C) 2017 The HDF Group
+//  All Rights Reserved 
+//
+// HOW TO RUN:
+//
+// $spark-2.3.0-SNAPSHOT-bin-hdf5s-0.0.1/bin/spark-shell -i demo.scala
+
+import org.hdfgroup.spark.hdf5._
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().appName("Spark SQL HDF5 example").getOrCreate()
+
+// Change the path name ('/tmp') if necessary.
+val df=spark.read.hdf5("/tmp/GSSTF_NCEP.3.2008.12.31.he5", "/HDFEOS/GRIDS/NCEP/Data Fields/SST")
+
+// Let's print some values from the dataset.
+df.show()
+
+// The output will look like below.
+//
+//+------+-----+------+
+//|FileID|Index| Value|
+//+------+-----+------+
+//|     0|    0|-999.0|
+//|     0|    1|-999.0|
+//|     0|    2|-999.0|
+//...
+
+System.exit(0)
