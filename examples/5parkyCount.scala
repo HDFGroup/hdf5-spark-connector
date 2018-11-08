79ac916f9f4a86dbf8f78a8e6e87f5aec6aa4fe7
Gerd Heber <gheber@hdfgroup.org>
2018-11-08 13:31:26 -0600

    Updated examples and run script.

--- examples/5parkyCount.scala
+++ examples/5parkyCount.scala
@@ -2,13 +2,18 @@
 
 import org.hdfgroup.spark.hdf5._
 
+sc.setLogLevel("ERROR")
+
 val dirName = "/mnt/wrk/hdftest/GSSTF_NCEP.3/2000"
 val varName = "/HDFEOS/GRIDS/NCEP/Data Fields/Tair_2m"
 
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-val df = sqlContext.read.option("extension", "he5").
-  option("window size", "200000").hdf5(dirName, varName)
+val df = { sqlContext.read
+  .option("extension", "he5")
+  . option("window size", "200000")
+  .hdf5(dirName, varName)
+}
 df.count()
 
 System.exit(0)
--- examples/blog.scala
+++ examples/blog.scala
@@ -1,24 +1,23 @@
 // INSTRUCTIONS TO RUN:
 // time spark-shell -i examples/blog.scala --jars target/scala-2.11/5parky_2.11-0.0.1-ALPHA.jar,lib/sis-jhdf5-batteries_included.jar --packages com.databricks:spark-csv_2.10:1.4.0
-// time spark-shell -i examples/blog.scala --jars target/scala-2.11/5parky_2.11-0.0.1-ALPHA.jar,lib/sis-jhdf5-batteries_included.jar --packages com.databricks:spark-avro_2.11:3.2.0
 // cat file.csv/*.csv > output.csv
 
-import com.databricks.spark.avro._
 import org.hdfgroup.spark.hdf5._
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
+
+sc.setLogLevel("ERROR")
 
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
 val dirName = "/mnt/wrk/hdftest/GSSTF_NCEP.3/2000"
 val varName = "/HDFEOS/GRIDS/NCEP/Data Fields/Tair_2m"
 
-val df = sqlContext.read.
-  option("extension", "he5").
-  option("window size", "200000").
-  hdf5(dirName, varName)
-
-// df.write.avro("df.avro")
+val df = { sqlContext.read
+  .option("extension", "he5")
+  .option("window size", "200000")
+  .hdf5(dirName, varName)
+}
 
 // extract the date from the file name
 val getDate = udf((file: String) =>
@@ -29,9 +28,19 @@ val nonFill = df.where($"value" > -999.0)
 
 // Top 10 and Bottom 10
 val w = Window.partitionBy($"fileID").orderBy($"value".desc)
-val dfTop = nonFill.withColumn("rn", row_number.over(w)).where($"rn" <= 10).drop("rn")
+val dfTop = { nonFill
+  .withColumn("rn", row_number.over(w))
+  .where($"rn" <= 10)
+  .drop("rn")
+}
+
 val w2 = Window.partitionBy($"fileID").orderBy($"value".asc)
-val dfTop2 = nonFill.withColumn("rn", row_number.over(w2)).where($"rn" <= 10).drop("rn")
+val dfTop2 = { nonFill
+  .withColumn("rn", row_number.over(w2))
+  .where($"rn" <= 10)
+  .drop("rn")
+}
+
 val dfTop3 = dfTop.unionAll(dfTop2)
 val values = collect_list($"value").alias("values")
 dfTop3.groupBy($"fileID").agg(values).show(false)
@@ -49,27 +58,33 @@ val df2 = sqlContext.sql(
      FROM df1 GROUP BY fileID""")
 df2.show(20)
 
-val idMap = sqlContext.read.
-  option("extension", "he5").hdf5(dirName, "sparky://files")
+val idMap = { sqlContext.read
+  .option("extension", "he5")
+  .hdf5(dirName, "sparky://files")
+}
 
-val dateMap = idMap.drop("FileSize").
-  withColumn("fileName", getDate($"FilePath"))
+val dateMap = { idMap
+  .drop("FileSize")
+  .withColumn("fileName", getDate($"FilePath"))
+}
 
-val meanEtMedian = df2.
-  join(dateMap, "fileID").
-  drop("fileID").
-  drop("cnt").
-  drop("min").
-  drop("max").
-  drop("stddev").
-  sort("fileName").
-  select("fileName", "mean", "median")
+val meanEtMedian = { df2
+  .join(dateMap, "fileID")
+  .drop("fileID")
+  .drop("cnt")
+  .drop("min")
+  .drop("max")
+  .drop("stddev")
+  .sort("fileName")
+  .select("fileName", "mean", "median")
+}
 
 meanEtMedian.show(20)
 
-meanEtMedian.write.
-  format("com.databricks.spark.csv").
-  option("header", "false").
-  save("file.csv")
+{ meanEtMedian
+  .write
+  .format("com.databricks.spark.csv")
+  .option("header", "false")
+  .save("file.csv") }
 
 System.exit(0)
